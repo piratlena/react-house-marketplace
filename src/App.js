4e826b9bcf373c3efb1a8f083aaf534fb3be2aa5
Elena Brazhnikova <lenovosibirsk@mail.ru>
2023-03-16 22:02:52 +0700

    implement contact apge

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import NavBar from "./components/NavBar";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListingsPage from "./pages/CreateListingsPage";
 import ListingPage from "./pages/ListingPage";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
           path="/category/:categoryName/:listingId"
           element={<ListingPage />}
         />
+        <Route path="/contact/:landlordId" element={<Contact />} />
       </Routes>
       <NavBar />
       <ToastContainer />
